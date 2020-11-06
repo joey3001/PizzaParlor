d5b641b6445c4fcb02a81914d1346236439b9a67
Joey3001 <josephnero111@gmail.com>
2020-11-06 10:02:18 -0800

    Create test spec for addToppings protoype.

--- tests/scripts.test.js
+++ tests/scripts.test.js
@@ -1,7 +1,15 @@
 // Describe: Pizza(size)
-
 // Test: Should correctly create an instance of Pizza
 // Code: let exampleSize = "large"
 // Code: let newPizza = new Pizza(exampleSize)
-// Code: let sizeResult = Pizza.size
-// Expect: sizeResult.toEqual("large")
+// Code: let result = Pizza.size
+// Expect: result.toEqual("large")
+
+// Describe: addToppings(toppings)
+// Test: Should correctly add toppings to an instance of Pizza
+// Code: let exampleSize = "large"
+// Code: let exampleToppings = ["mushrooms","onions","cheese"]
+// Code: let newPizza = new Pizza(exampleSize)
+// Code: newPizza.addToppings(exampleToppings)
+// Code: let result = newPizza.toppings
+// Expect: result.toEqual(["mushrooms","onions","cheese"])
