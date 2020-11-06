22c68e1c310a430ac60cbcc26abfa47166b716bd
Joey3001 <josephnero111@gmail.com>
2020-11-06 11:33:37 -0800

    Add feature to change cost based on # of toppings

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,9 @@ Pizza.prototype.costCalculator = function(){
   if (this.size === "large") {
     this.cost += 5; 
   }
+  if (this.toppings.length > 1) {
   return this.cost; 
+  }
 }
 
 let newPizza = new Pizza("large");
@@ -24,9 +26,4 @@ newPizza.addToppings(array);
 let newCost = newPizza.costCalculator();
 alert(newCost);
 
-
-
-
-
-
-
+alert(newPizza.toppings.length);
