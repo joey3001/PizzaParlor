a9fb67e51193b8e5889e98506ebc294dfd3cf071
Joey3001 <josephnero111@gmail.com>
2020-11-06 13:40:48 -0800

    Add function to store checkbox values in an array.

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ Pizza.prototype.addToppings = function (toppings) {
 }
 
 Pizza.prototype.costCalculator = function(){
-  if (this.size === "large") {
+  if (this.size === "Large") {
     this.cost += 5; 
   }
   if (this.toppings.length > 1) {
@@ -18,11 +18,22 @@ Pizza.prototype.costCalculator = function(){
   return this.cost; 
 }
 
+function toppingsArray(toppings) {
+  $("input:checkbox[name=toppings]:checked").each(function() {
+  toppings.push($(this).val());
+  });
+};
+
+
 $(document).ready(function() {
   $("#pizzaCost").submit(function(event) {
     event.preventDefault();
     let size = $("#size").val()
     let newPizza = new Pizza(size);
+    let toppings = []
+    toppingsArray(toppings);
+    newPizza.addToppings(toppings);
+    console.log(toppings);
     console.log(newPizza);
   });
 });
