6dbb75846b72cd250618f15fa034f547f7c3b4f6
Joey3001 <josephnero111@gmail.com>
2020-11-06 09:44:24 -0800

    Create 1st test spec in scripts.test.js file.

---
+++ tests/scripts.test.js
@@ -0,0 +1,7 @@
+// Describe: Pizza(size)
+
+// Test: Should correctly create an instance of Pizza
+// Code: let exampleSize = "large"
+// Code: let newPizza = new Pizza(exampleSize)
+// Code: let sizeResult = Pizza.size
+// Expect: sizeResult.toEqual("large")
