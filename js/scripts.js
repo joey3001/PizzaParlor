c3eb691921a6eabd4040758103b10237f6bf4ed5
Joey3001 <josephnero111@gmail.com>
2020-11-06 10:42:40 -0800

    Create test spec for costCalculator prototype in test file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
 function Pizza (size) {
   this.size = size; 
   this.toppings = []; 
+  this.cost = 0; 
+}
+
+Pizza.prototype.addToppings = function (toppings) {
+  this.toppings = toppings;
 }
 
 let newPizza = new Pizza ("large");
+
+let array = ["mushrooms","onions","apples"];
+
+newPizza.addToppings(array);
+
+console.log(newPizza.toppings);
+
+
+
--- tests/scripts.test.js
+++ tests/scripts.test.js
@@ -5,6 +5,12 @@
 // Code: let result = Pizza.size
 // Expect: result.toEqual("large")
 
+// Test: Should correctly create an instance of Pizza with a cost value of 5
+// Code: let exampleSize = "large"
+// Code: let newPizza = new Pizza(exampleSize)
+// Code: let result = Pizza.cost
+// Expect: result.toEqual(5)
+
 // Describe: addToppings(toppings)
 // Test: Should correctly add toppings to an instance of Pizza
 // Code: let exampleSize = "large"
@@ -13,3 +19,12 @@
 // Code: newPizza.addToppings(exampleToppings)
 // Code: let result = newPizza.toppings
 // Expect: result.toEqual(["mushrooms","onions","cheese"])
+
+// Describe: costCalculator
+// Test: Should add 5$ to the cost of the pizza if the pizza size if "large"
+// Code: let exampleSize = "large"
+// Code: let newPizza = new Pizza(exampleSize)
+// Code: let baseCost = newPizza.cost;
+// Code: newPizza.costCalculator()
+// Code: let result = newPizza.cost
+// Expect: result.toEqual(baseCost + 5)
