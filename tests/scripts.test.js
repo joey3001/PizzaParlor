ff3bd6c0e2addec6ffa501aec67677b06813c69e
Joey3001 <josephnero111@gmail.com>
2020-11-08 21:30:31 -0800

    Update formatting.

--- tests/scripts.test.js
+++ tests/scripts.test.js
@@ -7,7 +7,7 @@
 // Test: Should correctly create an instance of Pizza with input being provided to determine the size of the Pizza
 // Code: let exampleSize = "Large"
 // Code: let newPizza = new Pizza(exampleSize)
-// Code: let result = Pizza.size
+// Code: let result = newPizza.size
 // Expect: result.toEqual("Large")
 
 // Describe: toppingsArray
@@ -43,7 +43,7 @@
 // Expect: result.toEqual(baseCost + 5)
 
 // Test: Should add 5 to Pizza.cost if Pizza.toppings.length > 1
-// Code: let exampleSize = "small"
+// Code: let exampleSize = "Small"
 // Code: let newPizza = new Pizza(exampleSize)
 // Code: let exampleToppings = ["mushrooms","onions","cheese"]
 // Code: newPizza.addToppings(exampleToppings)
@@ -54,7 +54,7 @@
 
 // Describe: fadeFunction
 // Test: Should return a function that executes the fadeIn method on text associated with a specified selector & object.  
-// Code: let result = totalFade(selector, pizzaObject);
+// Code: let result = fadeFunction(selector, pizzaObject);
 /* Expect: result.toEqual( ƒ () {
     $(selector).text(pizzaObject.cost).fadeIn();
   })
